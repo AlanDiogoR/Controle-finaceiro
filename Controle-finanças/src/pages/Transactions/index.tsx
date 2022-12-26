6e7cc8c341f052f948f95b3b23343be15ee92b99
AlanDiogoR <alandiogor@gmail.com>
2022-12-26 14:29:57 -0300

    páginas estruuradas

--- Controle-finanças/src/pages/Transactions/index.tsx
+++ Controle-finanças/src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+import { SearchForm } from '../components/SearchForm';
+
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <>
       <Header/>
       <Summary/>
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>26/12/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Hamburgueres</td>
+              <td>
+                <PriceHighLight variant="outcome">-R$59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>26/12/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   );
 }
---
+++ Controle-finanças/src/pages/components/SearchForm/index.tsx
@@ -0,0 +1,16 @@
+import { MagnifyingGlass } from 'phosphor-react';
+
+import { SearchFormConatiner } from "./styles";
+
+export function SearchForm() {
+  return (
+    <SearchFormConatiner>
+      <input type="text" placeholder="Busque por transações" />
+
+      <button type="submit">
+        <MagnifyingGlass size={20}/>
+        Buscar
+      </button>
+    </SearchFormConatiner>
+  );
+}

d555a896b9a6051a53a61e53841dc1623e3c55cf
AlanDiogoR <alandiogor@gmail.com>
2022-12-27 21:46:08 -0300

    context criado e funcionando

--- Controle-finanças/src/pages/Transactions/index.tsx
+++ Controle-finanças/src/pages/Transactions/index.tsx
@@ -1,31 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { TransactionContext } from '../../contexts/TransactionsContex';
 import { SearchForm } from '../components/SearchForm';
 
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from './styles';
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createAt: string;
-}
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions');
-    const data = await response.json();
-
-    setTransactions(data);
-  }
-  useEffect(() => {
-    loadTransactions();
-  }, []);
+  const { transactions } = useContext(TransactionContext)
 
   return (
     <>
